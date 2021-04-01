8d328db4a7dd525b5bc905fd85cd63477c8a7123
Lukasz Piwowarski <lukaszp4@onet.eu>
2021-04-01 20:02:49 +0200

    add maven config files, aplications properties and spring boot application

---
+++ src/main/java/pl/piwowarski/lukasz/platerecognition/PlateRecognitionApplication.java
@@ -0,0 +1,14 @@
+package pl.piwowarski.lukasz.platerecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateRecognitionApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/pl/piwowarski/lukasz/platerecognition/PlateRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piwowarski.lukasz.platerecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
