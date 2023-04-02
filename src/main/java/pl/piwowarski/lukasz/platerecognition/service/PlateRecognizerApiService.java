8fc4cf4ad14c27c3b13b05e916927778b6d84501
Łukasz Piwowarski <lukaszp4@onet.eu>
2023-04-02 11:59:21 +0200

    added recognizer api client implementation with test

--- src/main/java/pl/piwowarski/lukasz/platerecognition/service/PlateRecognizerApiService.java
+++ src/main/java/pl/piwowarski/lukasz/platerecognition/service/PlateRecognizerApiService.java
@@ -1,8 +1,14 @@
 package pl.piwowarski.lukasz.platerecognition.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.logging.Logger;
 
@@ -11,12 +17,23 @@ public class PlateRecognizerApiService {
 
     private static final Logger LOGGER = Logger.getLogger(PlateRecognizerApiService.class.getName());
 
-    public void recognize(){
+    public String recognize(Resource resource) {
         LOGGER.info("recognize()");
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-        httpHeaders.add("Authorization"," Token ");
+        httpHeaders.add("Authorization", "Token c25bfb45c151a67d5c768988bdfd8ba197df9b75");
 
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("upload", resource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, httpHeaders);
+        String serverUrl = "https://api.platerecognizer.com/v1/plate-reader/";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+
+        LOGGER.info("recognize()=" + responseEntity);
+        return responseEntity.getBody();
     }
 
 }
